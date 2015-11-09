205d59cd43882ed3c274de69174b87836ce4e4b2
Dinar Rafikov <raaf98@mail.ru>
2015-11-09 13:03:55 +0300

    Create M2.pas

---
+++ M2.pas
@@ -0,0 +1,34 @@
+program M2;
+
+type
+    matrix = array [1..3, 1..3] of integer;
+
+var
+    a:  matrix;
+
+procedure ReadMatrix();
+var
+    i, j: integer;
+begin
+    for i := 1 to 3 do
+        for j := 1 to 3 do
+            readln(a[i, j]);
+end;
+
+procedure WriteMinLinMatrix(a: matrix);
+var
+    i, j, min: integer;
+begin
+    for i := 1 to 3 do 
+    begin
+        min := a[i, 1];
+        for j := 2 to 3 do 
+            if a[i, j] < min then min := a[i, j];
+        writeln('Строка', ' ', i, ' :', min);
+    end;
+end;
+
+begin
+    ReadMatrix();
+    WriteMinLinMatrix(a);
+end.
