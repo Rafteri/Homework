c5527850162f5e7d87cd583d8d35e25345fb45d7
Dinar Rafikov <raaf98@mail.ru>
2015-11-09 13:16:15 +0300

    Update M1.pas

--- M1.pas
+++ M1.pas
@@ -15,29 +15,18 @@ begin
             readln(a[i, j]);
 end;
 
-procedure TransposeMatrix(var a: matrix);
+procedure WriteTransposeMatrix(a: matrix);
 var
     i, j: integer;
 begin
-    for i := 2 to 3 do
-        for j := 1 to 2 do
-            swap(a[i, j], a[j, i]);
-end;
-
-procedure WriteMatrix();
-var
-    i, j: integer;
-begin
-    for i := 1 to 3 do 
-    begin
+    for i := 1 to 3 do begin
         for j := 1 to 3 do
-            write(a[i, j]);
+            write(a[j, i]);
         writeln();
     end;
 end;
 
 begin
     ReadMatrix();
-    TransposeMatrix(a);
-    WriteMatrix();
+    WriteTransposeMatrix(a);
 end.
