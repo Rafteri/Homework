384253621cab93f7acd508d1e5504b51987b2adf
Rafteri <raaf98@mail.ru>
2015-11-22 18:02:44 +0300

    Homework

--- Conversion.pas
+++ Conversion.pas
@@ -1,68 +1,38 @@
-program conversion;
+program conversions;
 
 var
   a: array of char;
-  i, x, n: integer;
-  b: boolean;
-  c: char;
+  i, n: integer;
 
 function is_digit(c: char): boolean;
 begin
-  if (c <= '9') and (c >= '0') 
-    then 
-  begin
-    is_digit := true;
-  end
-    else
-  begin
-    is_digit := false;
-  end;
+  is_digit := (c <= '9') and (c >= '0');
 end;
 
-function conversion(b: boolean; c: char): integer;
+function conversion(c: char): integer;
 begin
-  if b = true 
-    then 
-  begin
-    conversion := integer(c) - integer('0');
-  end
-    else
-  begin
-    conversion := -1;
-  end;
-end;
-
-function exponent(a, n: integer): integer;
-var
-  i, x: integer;
-begin
-  if n = 0 
-    then
-  begin
-    exponent := 1;
-  end
-    else
-  begin
-    x := 1;
-    for i := 1 to n do 
-    begin
-      x := x * a;
-    end;
-    exponent := x;
-  end;
+  conversion := (is_digit(c) ? integer(c) - integer('0') : -1);
 end;
 
 function converting(a: array of char; n: integer): integer;
 var
-  i, x, e, v: integer;
+  i, x, exp: integer;
 begin
-  x := 0;
-  for i := 0 to n - 1 do
+  x := 0; 
+  exp := 1;
+  for i := n - 1 downto 0 do
   begin
-    b := is_digit(a[i]);
-    v := conversion(b, a[i]);
-    e := exponent(10, n - 1 - i);
-    x := x + v * e;
+    if conversion(a[i]) = -1 
+    then
+    begin
+      converting := -1;
+      break;
+    end
+    else
+    begin
+      x := x + conversion(a[i]) * exp;
+      exp := exp * 10;
+    end;
   end;
   converting := x;
 end;
@@ -73,7 +43,12 @@ begin
   for i := 0 to n - 1 do 
   begin
     readln(a[i]);
+    if not is_digit(a[i]) 
+      then
+    begin
+      writeln(-1); 
+      exit; 
+    end;
   end;
-  x := converting(a, n);
-  writeln(x);
+  writeln(converting(a, n));
 end.
