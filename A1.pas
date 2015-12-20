5508bb2a4e7392973bbec4ebb0adb9bed8c0053d
Rafteri <raaf98@mail.ru>
2015-12-20 19:39:40 +0300

    Almost perfect code

--- A1.pas
+++ A1.pas
@@ -1,57 +1,56 @@
 program A1;
 
-uses
-  crt;
-
 var
   c: array of char;
   a, b: string;
-  lol, i, test: integer;
+  longest, i: integer;
 
 procedure sum(x, y: string; var c: array of char);
 
 var
   i, k: integer;
 begin
-  for i := lol downto 1 do
+  for i := longest downto 1 do
   begin
-     //writeln(i);
-    // writeln(c[i]);
-    // writeln(x[i]);
     c[i] := char(ord(c[i]) + ord(x[i]) - 48);
     c[i - 1] := char(((ord(c[i]) - 48) div 10) + 48);
     c[i] := char(((ord(c[i]) - 48) mod 10) + 48);
   end;
-  for i := lol downto lol - y.Length + 1 do
+  for i := longest downto longest - y.Length + 1 do
   begin
-    c[i] := char(ord(c[i]) + ord(y[i - lol + y.Length]) - 48);
+    c[i] := char(ord(c[i]) + ord(y[i - longest + y.Length]) - 48);
     c[i - 1] := char(ord(c[i - 1]) + ((ord(c[i]) - 48) div 10));
     c[i] := char(((ord(c[i]) - 48) mod 10) + 48);
   end;
-  if test > 9 then k := 0 else k := 1;
-  for i := k to lol do
+  if c[0] <> '0' then
+  begin
+    k := 0;
+  end
+  else 
+  begin
+    k := 1;
+  end;
+  for i := k to longest do
     write(c[i]);  
 end;
 
 begin
   readln(a);
   readln(b);
-  test := ord(a[1]) + ord(b[1]) - 96;
   if a.Length > b.length then
   begin
-    lol := a.Length;
-    setlength(c, lol + 1);
-    for i := 0 to lol do
+    longest := a.Length;
+    setlength(c, longest + 1);
+    for i := 0 to longest do
       c[i] := '0';
     sum(a, b, c);
   end
   else 
   begin
-    lol := b.Length;
-    setlength(c, lol + 1);
-    for i := 0 to lol do
+    longest := b.Length;
+    setlength(c, longest + 1);
+    for i := 0 to longest do
       c[i] := '0';
     sum(b, a, c); 
   end;
-  
 end.
