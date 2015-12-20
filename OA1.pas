968b46efed09d8b5e9fdf0026eb3976500af6124
Rafteri <raaf98@mail.ru>
2015-12-20 18:22:54 +0300

    Perfect code

--- OA1.pas
+++ OA1.pas
@@ -5,9 +5,21 @@ const N = 10;
 type arr_int = array [1..N] of integer;
 
 var a: arr_int;
-    i, num: integer;
+    num: integer;
 
-function dio_search(var a: arr_int):integer;
+procedure read_arr_int(var a: arr_int; var num: integer);
+var i:integer;
+begin
+  writeln('Enter ', N, ' elements');
+  for i:= 1 to N do
+  begin
+    readln(a[i]);
+  end;
+  writeln('Enter the required element');
+  readln(num);
+end;
+
+function dio_search(var a: arr_int; num: integer):integer;
 var i, lft, rght: integer;
     status: boolean;
 begin
@@ -16,11 +28,11 @@ begin
   while not status do
   begin
     i := (lft + rght) div 2;
-    if (a[i] < num) then
+    if (a[i] > num) then
     begin
       rght := i;
     end
-    else if (a[i] > num) then
+    else if (a[i] < num) then
     begin
       lft := i;
     end
@@ -29,16 +41,17 @@ begin
       dio_search := i;
       status := true;
     end;
+    if abs(lft - rght) = 1 then
+    begin
+      writeln('Element not found!');
+      break;
+    end
   end;
   
 
 end;
 
 begin
-  for i:= 1 to N do
-  begin
-    readln(a[i]);
-  end;
-  read(num);
-  writeln(dio_search(a));
+  read_arr_int(a, num);
+  writeln(dio_search(a, num));
 end.
